c9390c78ff5b4a8cba8008286729bc1f0f94ccba
Mohammad Ishan Ahmad <mdishanahmad@gmail.com>
2026-02-08 14:28:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ let i=0;
+ let j=0;
+	while(i<str.length && j<str.length)
+		{
+			while(str[i]==str[j])
+				{
+					j++;
+				}
+			if(j-i<2) return str[i]
+			i=j;
+		}
+	return null;
+	
 }
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+alert(firstNonRepeatedChar(input));
